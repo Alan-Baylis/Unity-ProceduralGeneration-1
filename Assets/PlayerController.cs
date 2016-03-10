14ed8edbe3fd76267fb3b53ab92dc4b69053a26b
Maly <holiwords@gmail.com>
2016-03-10 07:24:19 -0500

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    Vector3 velocity;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
+
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + velocity * Time.fixedDeltaTime);
+    }
+
+}
